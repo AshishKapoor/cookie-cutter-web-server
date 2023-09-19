127b9412149c147d1d0255a944c7ab411de2bd6a
Ashish Kapoor <swiftobjc@gmail.com>
2023-09-19 15:49:05 +0530

    clean up

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 8000;
+const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
+
+const PORT = process.env.PORT || 8000;
+
+// 3rd party middlewares
+app.use(cors()); // Cross Origin Resource Sharing
 
 // built-in middlewares
 app.use(express.urlencoded({ extended: false }));
@@ -12,45 +17,19 @@ app.use(express.static(path.join(__dirname, "/public")));
 // custom middleware logger
 app.use(logger);
 
+// home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get("/new-page(.html)?", (req, res) => {
+app.get("/new-page", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html");
+// redirect
+app.get("/old-page", (req, res) => {
+  res.redirect(301, "/new-page");
 });
-
-// Route Handlers
-app.get(
-  "/hello",
-  (req, res, next) => {
-    console.log("attempted to log /hello");
-    next();
-  },
-  (req, res) => {
-    res.send("hello");
-  }
-);
-
-function one(req, res, next) {
-  console.log("one !");
-  next();
-}
-function two(req, res, next) {
-  console.log("two !");
-  next();
-}
-function three(req, res, next) {
-  console.log("three !");
-  res.send("DONE!");
-}
-
-// middleware
-app.get("/chain", [one, two, three]);
 
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
